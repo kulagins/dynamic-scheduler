7bb04ab311d2fc2aaf1f73d3bd8d01b30c73d50f
kulagins <kulagins@hu-berllin.de>
2024-09-30 12:29:10 +0200

    experiments

--- experiment.sh
+++ experiment.sh
@@ -3,17 +3,6 @@ pwd
 #!/bin/bash
 
 echo "Arguments passed to the script: $*"
-# Call the fonda_scheduler with no parameters
-./build/fonda_scheduler
-
-# Check if the scheduler ran successfully
-if [ $? -ne 0 ]; then
-    echo "fonda_scheduler failed"
-    exit 1
-fi
-
-# Sleep for 2 seconds before running the Python script
-sleep 2
 
 cd ../runtime-system || exit
 
@@ -25,5 +14,18 @@ if [ $? -ne 0 ]; then
     echo "Python script failed"
     exit 1
 fi
+
+
+# Call the fonda_scheduler with no parameters
+./build/fonda_scheduler
+
+# Check if the scheduler ran successfully
+if [ $? -ne 0 ]; then
+    echo "fonda_scheduler failed"
+    exit 1
+fi
+
+# Sleep for 2 seconds before running the Python script
+sleep 2
 
 echo "Both executables ran successfully."
