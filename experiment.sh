27bcd42f56435564596fe5b37ad1ddfa123ece33
kulagins <kulagins@hu-berllin.de>
2024-09-30 11:55:34 +0200

    experiments

---
+++ experiment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+pwd
+#!/bin/bash
+
+# Call the fonda_scheduler with no parameters
+./build/fonda_scheduler
+
+# Check if the scheduler ran successfully
+if [ $? -ne 0 ]; then
+    echo "fonda_scheduler failed"
+    exit 1
+fi
+
+# Sleep for 2 seconds before running the Python script
+sleep 2
+
+cd ../runtime-system || exit
+
+# Run the python script with all the arguments passed to this bash script
+python -m gear "$@"
+
+# Check if the python script ran successfully
+if [ $? -ne 0 ]; then
+    echo "Python script failed"
+    exit 1
+fi
+
+echo "Both executables ran successfully."
